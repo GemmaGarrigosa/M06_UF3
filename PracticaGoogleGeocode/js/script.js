ecc93c3beb3b1da5dee441c8bfc52a16d27fcb06
Gemma <gemma.garrigosa@gmail.com>
2024-03-05 18:12:02 +0100

    Acabada part obligatoria de la pràctica

--- PracticaGoogleGeocode/js/script.js
+++ PracticaGoogleGeocode/js/script.js
@@ -1,11 +1,12 @@
-let map
-let button = document.querySelector("button");
+let map;
+let button = document.querySelector("#findLoc");
+let marcador = document.querySelector("#marcador");
 
 async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
-    const center ={ lat: 41.390205, lng: 2.154007 },
+    const center ={ lat: 41.390205, lng: 2.154007 };
   map = new Map(document.getElementById("map"), {
-    center,
+    center: center,
     zoom: 15,
   });
 
@@ -17,18 +18,16 @@ async function initMap() {
 
 }
 
-initMap();
 
 button.addEventListener("click", mostraCoordenades);
 
-
+marcador.addEventListener("click",mouMarcador);
 
 
 
 function mostraCoordenades() {
   //Revem el valor del input de adreça
     let adreca = document.getElementsByName('adreca')[0].value;
-    alert(adreca);
     let geocoder = new google.maps.Geocoder();
     let address=adreca;
     geocoder.geocode( { 'address': address}, function(results, status) {
@@ -36,11 +35,45 @@ function mostraCoordenades() {
         if (status == google.maps.GeocoderStatus.OK) {
             latitude = results[0].geometry.location.lat();
             longitude = results[0].geometry.location.lng();
-           
-            console.log(`aquesta es la latitud: ${latitude} i aquesta es la longitut: ${longitude}`);
+
+            let latitut = document.getElementById("latitude");
+            let longitut = document.getElementById("longitude");
+
+            latitut.value = latitude;
+            longitut.value = longitude;
             
-            
+            let pos = new google.maps.LatLng(latitude, longitude);
+            map.setCenter(pos);
+            map.setZoom(16);
+
+            new google.maps.Marker({
+              position: pos,
+              map: map,
+              title: "Ubicació trobada!"
+          });
+          
+         } else {
+           alert("No s'ha trobat la direcció");
          }
     });
     
 }
+
+function mouMarcador() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      let pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map.setCenter(pos);
+      map.setZoom(9);
+       new google.maps.Marker({
+        position: pos,
+        map: map
+      });
+    });
+  }
+}
+
+initMap();
